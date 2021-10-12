55c759d7345bd6ea526e05169279f9d9379c0bb1
Flavius Dolca <f.dolca@gmail.com>
2021-10-12 02:31:50 +0200

    Fix code

--- template/api/article/models/article.js
+++ template/api/article/models/article.js
@@ -1,6 +1,5 @@
 'use strict';
-const { isDraft } = require('strapi-utils').contentTypes;
-
+const axios = require('axios');
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
@@ -8,6 +7,17 @@ const { isDraft } = require('strapi-utils').contentTypes;
 
 module.exports = {
   lifecycles: {
+    async afterCreate(article) {
+      const slugIdPatternRegex = /-\d+\.\d+?$/
+      const slugContainsId = slugIdPatternRegex.test(article.slug)
+      console.log(slugContainsId)
+      if (!slugContainsId) {
+        const publinfo = await strapi.query('publinfo').findOne();
+        article.slug = article.slug + "-" + publinfo.idPublication + "." + article.id
+      }
+      console.log(article)
+      await strapi.services.article.update({ id: article.id }, article)
+    },
     async afterUpdate(updatedArticle) {
 
       if (updatedArticle.published_at == null) {
@@ -16,26 +26,76 @@ module.exports = {
         console.log("!isDraft")
         const publinfo = await strapi.query('publinfo').findOne();
 
-        console.log(JSON.stringify({
+        const result = {
           article: {
             title: updatedArticle.title,
             description: updatedArticle.description,
             content: updatedArticle.content,
             slug: updatedArticle.slug,
-            category: updatedArticle.category,
-            author: updatedArticle.author,
+            category: updatedArticle.categories,
+            subject: updatedArticle.subjects,
+            author: updatedArticle.authors,
+            image: updatedArticle.image,
+            gallery: updatedArticle.gallery,
             published_at: updatedArticle.published_at,
-            created_at: updatedArticle.created_at
+            created_at: updatedArticle.created_at,
+            link: "https://" + publinfo.fqdn + "/" + updatedArticle.slug,
+            guid: "https://" + publinfo.fqdn + "/" + updatedArticle.slug,
+            objectID: "https://" + publinfo.fqdn + "/" + updatedArticle.slug,
+            partners_id: updatedArticle.slug.match(/\d+\.\d+?$/) || null,
+            comments: true,
+            type: "article",
+            unixtimestamp: Date.now()
           },
           publinfo: {
             name: publinfo.name,
             slug: publinfo.slug,
             fqdn: publinfo.fqdn,
-            department: publinfo.department,
+            departament: publinfo.properties.algolia.departament,
+            site: publinfo.properties.algolia.site,
+            edition: publinfo.properties.algolia.edition,
+            idPublication: publinfo.idPublication,
             template: publinfo.template,
+            source: publinfo.source,
+
           },
-        }))
+          method: "write"
+        }
+        console.log(JSON.stringify(result))
+
+        let axiosConfig = {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+            "Connection": "keep-alive",
+          }
+        }
+        const r = await axios.post(process.env.POLPLEXER, JSON.stringify(result), axiosConfig)
+        console.log(r.data)
       }
     },
+    async beforeDelete(params) {
+
+      const art = await strapi.services.article.findOne(params)
+      const publinfo = await strapi.query('publinfo').findOne();
+
+      console.log(art)
+      const result = {
+        method: "delete",
+        parameters: {
+          guid: "https://" + publinfo.fqdn + "/" + art.slug,
+        }
+      }
+
+      let axiosConfig = {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+          "Connection": "keep-alive",
+        }
+      }
+      const r = await axios.post(process.env.POLPLEXER, JSON.stringify(result), axiosConfig)
+      console.log(r.data)
+    }
   },
 };
